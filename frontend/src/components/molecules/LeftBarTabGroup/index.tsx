1b7bb2e45e67e2bf6bfebeb759eb87fc42e21a02
danieldeng2 <danieldeng223@gmail.com>
2020-08-03 22:32:24 +0100

    Refactor left bar to use css modules

--- frontend/src/components/molecules/LeftBarTabGroup/index.tsx
+++ frontend/src/components/molecules/LeftBarTabGroup/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import styles from "./style.module.scss";
 
 const LeftBarTabGroup: React.FC = () => {
   return (
     <>
-      <h1 className="module-outline-heading">Sort</h1>
+      <h1 className={styles.moduleOutlineHeading}>Sort</h1>
       <div
-        className="btn-group-vertical"
+        className={"btn-group-vertical " + styles.moduleOutlineButtonGroup}
         role="group"
-        id="module-outline-button-group"
       >
-        <Button className="module-outline-button">Progress</Button>
-        <Button className="module-outline-button-a active" type="button">
+        <Button className={styles.moduleOutlineButton}>Progress</Button>
+        <Button className={"active " + styles.moduleOutlineButton}>
           Module Title
         </Button>
-        <Button className="module-outline-button">Module Code</Button>
-        <Button className="module-outline-button">Term</Button>
+        <Button className={styles.moduleOutlineButton}>Module Code</Button>
+        <Button className={styles.moduleOutlineButton}>Term</Button>
       </div>
     </>
   );
--- frontend/src/components/organisms/LeftBar/index.tsx
+++ frontend/src/components/organisms/LeftBar/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import LeftBarTabGroup from "components/molecules/LeftBarTabGroup";
+import styles from "./style.module.scss"
 
 const LeftBar: React.FC = () => {
   return (
-    <div id="sidebar-wrapper">
-      <p className="sidebar-status">1 UPDATE</p>
-
+    <div id={styles.sidebarWrapper}>
+      <p className={styles.sidebarStatus}>1 UPDATE</p>
       <LeftBarTabGroup />
     </div>
   );
