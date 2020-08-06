a989d6bab3d8af5e91bdfb010144718bb893e80c
danieldeng2 <danieldeng223@gmail.com>
2020-08-06 22:18:07 +0100

    generates calendar cards from array

--- frontend/src/components/molecules/CalendarGroup/index.tsx
+++ frontend/src/components/molecules/CalendarGroup/index.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import styles from "./style.module.scss";
-import Card from "react-bootstrap/Card";
 import classNames from "classnames";
+import CalendarCard, { eventTypes, CalendarCardProps } from "components/atoms/CalendarCard";
 
 const CalendarGroup: React.FC = () => {
+  let events: CalendarCardProps[] = [
+    {
+      type: eventTypes.Lecture,
+      title: "CO145",
+      subtitle: "13:00 - 14:00",
+      content: "311, Huxley Building, South Kensington Campus",
+    },
+    {
+      type: eventTypes.Labs,
+      title: "CO161",
+      subtitle: "15:00 - 17:00",
+      content: "219, Huxley Building, South Kensington Campus",
+    },
+    {
+      type: eventTypes.Deadline,
+      title: "CO120.1",
+      subtitle: "19:00",
+      content: "Haskell L Systems",
+    },
+  ];
+
   return (
     <>
       <h1 className={styles.calendarCardGroupHeading}>Today</h1>
@@ -11,40 +32,15 @@ const CalendarGroup: React.FC = () => {
         className={classNames("btn-group-vertical", styles.calendarCardGroup)}
         role="group"
       >
-        <Card
-          className={classNames(styles.calendarCard, styles.calendarLecture)}
-        >
-          <Card.Body>
-            <Card.Title>Lecture: CO142</Card.Title>
-            <Card.Subtitle>09:00 - 11:00</Card.Subtitle>
-            <Card.Text>308, Huxley Building, South Kensington Campus</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card
-          className={classNames(styles.calendarCard, styles.calendarLecture)}
-        >
-          <Card.Body>
-            <Card.Title>Lecture: CO145</Card.Title>
-            <Card.Subtitle>13:00 - 14:00</Card.Subtitle>
-            <Card.Text>311, Huxley Building, South Kensington Campus</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className={classNames(styles.calendarCard, styles.calendarLabs)}>
-          <Card.Body>
-            <Card.Title>Labs: CO161</Card.Title>
-            <Card.Subtitle>15:00 - 17:00</Card.Subtitle>
-            <Card.Text>219, Huxley Building, South Kensington Campus</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card
-          className={classNames(styles.calendarCard, styles.calendarDeadline)}
-        >
-          <Card.Body>
-            <Card.Title>Deadline: CO120.1</Card.Title>
-            <Card.Subtitle>19:00</Card.Subtitle>
-            <Card.Text>Haskell L Systems</Card.Text>
-          </Card.Body>
-        </Card>
+        {events.map(({ type, title, subtitle, content }) => (
+          <CalendarCard
+            title={title}
+            type={type}
+            subtitle={subtitle}
+						content={content}
+						key={title + type + subtitle}
+          />
+        ))}
       </div>
     </>
   );
