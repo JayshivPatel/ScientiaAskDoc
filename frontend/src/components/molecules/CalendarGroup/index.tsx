3cfce079d28767dfc8acf002fa41ff8f637eb1e6
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-08-06 20:26:01 +0530

    Update styling of calendar view

--- frontend/src/components/molecules/CalendarGroup/index.tsx
+++ frontend/src/components/molecules/CalendarGroup/index.tsx
@@ -1,22 +1,48 @@
 import React from "react";
 import styles from "./style.module.scss";
 import Card from "react-bootstrap/Card";
+import classNames from "classnames";
 
 const CalendarGroup: React.FC = () => {
   return (
     <>
-      <h1 className={styles.tabGroupHeading}>Today</h1>
+      <h1 className={styles.calendarCardGroupHeading}>Today</h1>
       <div
-        className={"btn-group-vertical " + styles.tabGroupButtonGroup}
+        className={classNames("btn-group-vertical", styles.calendarCardGroup)}
         role="group"
       >
-        <Card>
+        <Card
+          className={classNames(styles.calendarCard, styles.calendarLecture)}
+        >
           <Card.Body>
             <Card.Title>Lecture: CO142</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Subtitle>09:00 - 11:00</Card.Subtitle>
+            <Card.Text>308, Huxley Building, South Kensington Campus</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card
+          className={classNames(styles.calendarCard, styles.calendarLecture)}
+        >
+          <Card.Body>
+            <Card.Title>Lecture: CO145</Card.Title>
+            <Card.Subtitle>13:00 - 14:00</Card.Subtitle>
+            <Card.Text>311, Huxley Building, South Kensington Campus</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className={classNames(styles.calendarCard, styles.calendarLabs)}>
+          <Card.Body>
+            <Card.Title>Labs: CO161</Card.Title>
+            <Card.Subtitle>15:00 - 17:00</Card.Subtitle>
+            <Card.Text>219, Huxley Building, South Kensington Campus</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card
+          className={classNames(styles.calendarCard, styles.calendarDeadline)}
+        >
+          <Card.Body>
+            <Card.Title>Deadline: CO120.1</Card.Title>
+            <Card.Subtitle>19:00</Card.Subtitle>
+            <Card.Text>Haskell L Systems</Card.Text>
           </Card.Body>
         </Card>
       </div>
