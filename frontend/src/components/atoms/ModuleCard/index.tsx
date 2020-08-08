c63273905e0bc5e9dc109fe5ebfa4c6a7ec89aef
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-08-08 15:55:30 +0530

    Update module card styling
    Note: The header of the card was updated to include an icon for the
    module term and the footer was updated to include the percentage of
    completion.

--- frontend/src/components/atoms/ModuleCard/index.tsx
+++ frontend/src/components/atoms/ModuleCard/index.tsx
@@ -4,34 +4,39 @@ import styles from "./style.module.scss";
 import classNames from "classnames";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
+import { faLeaf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export interface ModuleCardProps {
-  module: {title: string;
-  code: string;
-  image: string;
-  content: string;}
+  module: { title: string; code: string; image: string; content: string };
 }
 
-const ModuleCard: React.FC<ModuleCardProps> = ({
-  module
-}: ModuleCardProps) => {
+const ModuleCard: React.FC<ModuleCardProps> = ({ module }: ModuleCardProps) => {
   return (
     <Col xs={12} sm={6} lg={4} xl={3} style={{ marginTop: "1.875rem" }}>
-      <Card className={classNames(styles.moduleCard)}>
-        <Link style={{ textDecoration: "none" }} to={`modules/${module.code}`}>
-        <Card.Header className={classNames(styles.moduleCardHeader)}>
-          {module.code}
+      <Card
+        className={classNames(styles.moduleCard)}
+        as={Link}
+        to={`modules/${module.code}`}
+      >
+        <Card.Header>
+          <FontAwesomeIcon icon={faLeaf}/>
+          <span>{module.code}</span>
         </Card.Header>
-        <Card.Img style={{ borderRadius: 0 }} variant="top" src={module.image} />
+        <Card.Img
+          style={{ borderRadius: 0 }}
+          variant="top"
+          src={module.image}
+        />
         <Card.Body>
           <Card.Title style={{ color: "#000" }}>{module.title}</Card.Title>
         </Card.Body>
         <Card.Footer>
-          <small className={classNames(styles.moduleCardProgressText)}>
+          <span className={classNames(styles.moduleCardProgressText)}>
             in progress
-          </small>
+          </span>
+          <span className={classNames(styles.moduleCardProgressText)}>60%</span>
         </Card.Footer>
-          </Link>
       </Card>
     </Col>
   );
