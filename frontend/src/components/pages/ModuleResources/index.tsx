a581b7ba299b4ef612855deb03e0fd45221bcb1e
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-08-10 21:29:37 +0530

    Update styling of resources page

--- frontend/src/components/pages/ModuleResources/index.tsx
+++ frontend/src/components/pages/ModuleResources/index.tsx
@@ -4,13 +4,16 @@ import styles from "./style.module.scss";
 import classNames from "classnames";
 import MyBreadcrumbs from "components/atoms/MyBreadcrumbs";
 
+import graphIllustration from "assets/images/graph-illustration.svg";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faInfoCircle, faFile } from "@fortawesome/free-solid-svg-icons";
 
 const ModuleResources: React.FC = () => {
   useEffect(() => {
@@ -21,14 +24,17 @@ const ModuleResources: React.FC = () => {
   return (
     <>
       <MyBreadcrumbs />
-      <InputGroup  style={{ marginTop: "3rem" }}>
-        <InputGroup.Prepend>
-          <InputGroup.Text>
-            <FontAwesomeIcon icon={faSearch} />
-          </InputGroup.Text>
-        </InputGroup.Prepend>
-
-        <FormControl aria-label="Search" placeholder="Search" />
+      <InputGroup>
+        <FormControl
+          className={styles.searchBar}
+          aria-label="Search"
+          placeholder="Search..."
+        />
+        <InputGroup.Append>
+          <Button className={styles.searchBarIcon}>
+            <FontAwesomeIcon size="1x" icon={faInfoCircle} />
+          </Button>
+        </InputGroup.Append>
       </InputGroup>
 
       <h5 className={classNames(styles.moduleSectionHeader)}>Quick Access</h5>
@@ -36,23 +42,41 @@ const ModuleResources: React.FC = () => {
       <Row>
         {[...Array(4)].map((e, i) => (
           <Col xs={6} sm={6} md={3} key={i}>
-            <Card style={{ marginTop: "1rem" }}>
-              <Card.Img variant="top" src="holder.js/100px100" />
+            <Card className={styles.quickViewCard}>
+              <Card.Img variant="top" src={graphIllustration} />
               <Card.Body>
                 <Card.Title>Document {i}</Card.Title>
+                <FontAwesomeIcon
+                  style={{ fontSize: "1.125rem" }}
+                  icon={faFile}
+                />
               </Card.Body>
+              <Card.Footer>
+                <Badge pill className={styles.quickViewTag}>
+                  New
+                </Badge>
+              </Card.Footer>
             </Card>
           </Col>
         ))}
       </Row>
 
-      <h5 className={classNames(styles.moduleSectionHeader)}>Folders</h5>
+      <h5
+        style={{ marginTop: "30px", marginBottom: "10px" }}
+        className={classNames(styles.moduleSectionHeader)}
+      >
+        Folders
+      </h5>
       <Row>
         {[...Array(10)].map((e, i) => (
           <Col xs={6} sm={6} md={3} key={i}>
-            <Card style={{ marginTop: ".6rem" }}>
+            <Card className={styles.folderCard}>
               <Card.Body style={{ padding: ".6rem" }}>
-                <Card.Text>Folder{i}</Card.Text>
+                <Card.Text style={{ marginBottom: 0 }}>Folder {i}</Card.Text>
+                <FontAwesomeIcon
+                  style={{ fontSize: "1.125rem" }}
+                  icon={faFile}
+                />
               </Card.Body>
             </Card>
           </Col>
