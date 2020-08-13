1c25c32128d49538cdf1c4f00bda9c57628ab817
danieldeng2 <danieldeng223@gmail.com>
2020-08-13 22:50:22 +0100

    More relavent notice board

--- frontend/src/components/organisms/NoticeBoard/index.tsx
+++ frontend/src/components/organisms/NoticeBoard/index.tsx
@@ -16,24 +16,24 @@ export default NoticeBoard;
 
 const noticeItems = [
   {
-    heading: "Lab Closure Information",
+    heading: "Foyer Closure Information",
     user: "Konstantinos Gkotuzis",
     time: "1 Minute Ago",
     body:
-      "Computing lab rooms 209, 208, and 210 would be closed for the year one haskell exams on Friday 20th of April.",
+      "The foyer area, along with computing lab rooms 209, 208, and 210 would be closed due to unforeseen circumstances on Friday 20th of April.",
   },
   {
     heading: "Dead body found in foyer area",
     user: "Orenthal James Simpson",
     time: "1 hour Ago",
     body:
-      "Whoever left the dead body of Rosalind Baker in the foyer area could you please clean after youself, thank you. Nobody needs to see that. ",
+      "Whoever left the dead body of Dr Rosalind Baker in the foyer area could you please cleanup after youself, thank you. Nobody needs to see that. ",
   },
   {
     heading: "Autumn Fee Information",
     user: "Rosalind Baker",
     time: "5 hours Ago",
     body:
-      "We understand that a lot of you would not be on campus for the autmumn term, however that does not mean that you should stop paying us. Good day. ",
+      "We understand that a lot of you would not be on campus for the autumn term, however that does not mean that you should stop paying us. Good day. ",
   },
 ];
