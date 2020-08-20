d673d5acf4b42181c5c6c6deb9ede791fa456713
danieldeng2 <danieldeng223@gmail.com>
2020-08-20 01:00:48 +0100

    Reimplement folder redirection as router hooks

--- src/components/pages/ModuleResources/components/FoldersView.tsx
+++ src/components/pages/ModuleResources/components/FoldersView.tsx
@@ -4,6 +4,7 @@ import SelectionView, {
   SelectionProps,
 } from "components/molecules/SelectionView";
 import FoldersRow from "components/molecules/FoldersRow";
+import { useHistory, useLocation } from "react-router-dom";
 
 export interface FoldersViewProps {
   resources: Resource[];
@@ -16,24 +17,31 @@ const FoldersView: React.FC<FoldersViewProps> = ({
   scope,
   searchText,
 }) => {
-	let folders: { title: string; id: number }[] = Array.from(
-		new Set<string>(resources.map((res: Resource) => res.folder))
-	).map((title: string, id: number) => ({
-		title: title,
-		id: id,
-	}));
+  let history = useHistory();
+  let location = useLocation();
+  let folders: { title: string; id: number }[] = Array.from(
+    new Set<string>(resources.map((res: Resource) => res.folder))
+  ).map((title: string, id: number) => ({
+    title: title,
+    id: id,
+  }));
 
-	if (searchText === "" && scope === "" && folders.length > 0) {
-		return (
-			<SelectionView
-				heading="Folders"
-				onDownloadClick={() => {}}
-				onItemClick={() => {}}
-				selectionItems={folders}
-				render={(select: SelectionProps) => <FoldersRow select={select} />}
-			/>
-		);
-	}
+  function handleFolderClick(foldersId: number) {
+    let title = folders.filter(({ id }) => id === foldersId)[0].title;
+    history.push(`${location.pathname}/${title}`);
+  }
+
+  if (searchText === "" && scope === "" && folders.length > 0) {
+    return (
+      <SelectionView
+        heading="Folders"
+        onDownloadClick={() => {}}
+        onItemClick={handleFolderClick}
+        selectionItems={folders}
+        render={(select: SelectionProps) => <FoldersRow select={select} />}
+      />
+    );
+  }
   return null;
 };
 
