efca4a412450d748ab94b9b0a9fc3a0859dffd51
danieldeng2 <danieldeng223@gmail.com>
2020-09-07 15:57:43 +0100

    Add tags to given files

--- src/components/organisms/EventModal/index.tsx
+++ src/components/organisms/EventModal/index.tsx
@@ -26,21 +26,21 @@ const EventModal: React.FC<Props> = ({ event, show, onHide }) => {
       <Modal.Header className={styles.modalHeader}>
         <Modal.Title>{event.prefix}</Modal.Title>
         <div style={{ display: "flex" }}>
-        <Button
-          variant="secondary"
-					className={styles.sectionHeaderButton}
-					href={`mailto:${event.owner}@ic.ac.uk?subject=Regarding ${event.title}`}
-					target="_blank"
-        >
-          <FontAwesomeIcon className={styles.buttonIcon}  icon={faEnvelope} />
-        </Button>
-        <Button
-          variant="secondary"
-          className={styles.sectionHeaderButton}
-          onClick={onHide}
-        >
-          <FontAwesomeIcon className={styles.buttonIcon} icon={faTimes} />
-        </Button>
+          <Button
+            variant="secondary"
+            className={styles.sectionHeaderButton}
+            href={`mailto:${event.owner}@ic.ac.uk?subject=Regarding ${event.title}`}
+            target="_blank"
+          >
+            <FontAwesomeIcon className={styles.buttonIcon} icon={faEnvelope} />
+          </Button>
+          <Button
+            variant="secondary"
+            className={styles.sectionHeaderButton}
+            onClick={onHide}
+          >
+            <FontAwesomeIcon className={styles.buttonIcon} icon={faTimes} />
+          </Button>
         </div>
       </Modal.Header>
       <Modal.Body className={styles.modalBody}>
@@ -50,40 +50,46 @@ const EventModal: React.FC<Props> = ({ event, show, onHide }) => {
           <span className={styles.eventStatus}>{event.status}</span>
         </div>
         <div className={styles.eventTimeInfo}>
-          <span className={styles.eventTime}>Issued: {event.startDate.toLocaleDateString()}</span>
-          <span className={styles.eventTime}>Due: {event.endDate.toLocaleDateString()}</span>
+          <span className={styles.eventTime}>
+            Issued: {event.startDate.toLocaleDateString()}
+          </span>
+          <span className={styles.eventTime}>
+            Due: {event.endDate.toLocaleDateString()}
+          </span>
         </div>
         <h4>Given</h4>
-        {dummy.map(({ title, type, id }: any) => (
-                    <FileListItem
-                      title={title}
-                      tags={[]}
-                      icon={resourceTypeToIcon(type)}
-                      onClick={() => {}}
-                      key={id}
-                    />
-                  ))}
-       </Modal.Body>
-       <Modal.Footer className={styles.modalFooter}>
-       <Button variant="secondary" className={styles.submitButton}>
-         Submit
-       </Button>
-       </Modal.Footer>
+        {dummy.map(({ title, type, tags, id }: any) => (
+          <FileListItem
+            title={title}
+            tags={tags}
+            icon={resourceTypeToIcon(type)}
+            onClick={() => {}}
+            key={id}
+          />
+        ))}
+      </Modal.Body>
+      <Modal.Footer className={styles.modalFooter}>
+        <Button variant="secondary" className={styles.submitButton}>
+          Submit
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
 
-const dummy = [ 
-  { 
-    title: "Problem Specification",
+const dummy = [
+  {
+    title: "spec-112-1.pdf",
     type: "pdf",
-    id: 1
+    tags: ["Specification"],
+    id: 1,
   },
-  { 
-    title: "Model Answer",
+  {
+    title: "task1_ans.pdf",
+    tags: ["Solution"],
     type: "pdf",
-    id: 2
-  }
-]
+    id: 2,
+  },
+];
 
 export default EventModal;
