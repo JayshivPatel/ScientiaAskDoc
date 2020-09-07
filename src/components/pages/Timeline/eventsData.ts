7bc4735b74c7b043590feec121cf1d561ad369d7
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-09-07 13:05:01 +0530

    Add more events to the timeline

--- src/components/pages/Timeline/eventsData.ts
+++ src/components/pages/Timeline/eventsData.ts
@@ -3,16 +3,40 @@ import { TimelineEvent } from ".";
 
 export const eventsData : TimelineEvent[] = [
 	{
-		title: "Logic Exercise",
+		title: "PMT: Syntax and Semantics of Propositional Logic",
 		id: 0,
 		startDate: new Date("2020-10-07"),
-		endDate: new Date("2020-10-19"),
+		endDate: new Date("2020-10-14"),
 		type: "tutorial",
 		moduleCode: "CO140"
 	},
 	{
-		title: "Coursework 1",
+		title: "PMT: Introduction to First Order Logic",
 		id: 1,
+		startDate: new Date("2020-10-14"),
+		endDate: new Date("2020-10-21"),
+		type: "coursework",
+		moduleCode: "CO140"
+	},
+	{
+		title: "PMT: Semantics of First Order Logic",
+		id: 2,
+		startDate: new Date("2020-10-21"),
+		endDate: new Date("2020-10-28"),
+		type: "tutorial",
+		moduleCode: "CO140"
+	},
+	{
+		title: "PMT: Semantics of FOL and Translation",
+		id: 3,
+		startDate: new Date("2020-10-28"),
+		endDate: new Date("2020-11-04"),
+		type: "coursework",
+		moduleCode: "CO140"
+	},
+	{
+		title: "Coursework 1",
+		id: 4,
 		startDate: new Date("2020-10-05"),
 		endDate: new Date("2020-10-12"),
 		type: "coursework",
@@ -20,7 +44,7 @@ export const eventsData : TimelineEvent[] = [
 	},
 	{
 		title: "Practical 1",
-		id: 2,
+		id: 5,
 		startDate: new Date("2020-10-05"),
 		endDate: new Date("2020-10-16"),
 		type: "tutorial",
