2c4d36f4cb2583a109bc200ddbdd1355cd89a4fd
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-09-08 14:06:47 +0530

    Update styling of the submissions page

--- src/components/pages/ModuleSubmissions/index.tsx
+++ src/components/pages/ModuleSubmissions/index.tsx
@@ -3,6 +3,15 @@ import Dandruff from "components/molecules/Dandruff";
 import EventModal from "components/organisms/EventModal";
 import { TimelineEvent } from "constants/types";
 import { eventsData } from "../Timeline/eventsData";
+import styles from "./style.module.scss";
+import Row from "react-bootstrap/esm/Row";
+import MyBreadcrumbs from "components/atoms/MyBreadcrumbs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBullhorn,
+  faExclamationCircle,
+  faCheckCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   moduleID: string;
@@ -12,28 +21,91 @@ const ModuleSubmissions: React.FC<Props> = ({ moduleID }) => {
   let [activeEvent, setActiveEvent] = useState<TimelineEvent | undefined>(
     undefined
   );
+
   return (
     <>
-      <Dandruff heading="Submissions" />
+      <MyBreadcrumbs />
       <EventModal
         event={activeEvent}
         show={showModal}
         onHide={() => setShowModal(false)}
         activeDay={new Date("2020-10-12")}
       />
-      {eventsData
-        .filter(({ moduleCode }) => moduleCode === moduleID)
-        .map((e) => (
-          <p
-            key={e.id}
-            onClick={() => {
-              setActiveEvent(e);
-              setShowModal(true);
-            }}
-          >
-            {e.title}
-          </p>
-        ))}
+      <div className={styles.listContainer}>
+        {eventsData
+          .filter(({ moduleCode }) => moduleCode === moduleID)
+          .map((e) => {
+            let icon = undefined;
+            let cardColour = "blue";
+            let borderColour = "";
+            switch (e.assessment) {
+              case "unassessed submission":
+                cardColour = "blue";
+                break;
+              case "individual assessed":
+                cardColour = "teal";
+                break;
+              case "group assessed":
+                cardColour = "pink";
+                break;
+              case "unassessed":
+                cardColour = "cyan";
+                break;
+              case "written exam":
+                cardColour = "indigo";
+                break;
+            }
+            switch (e.status) {
+              case "due":
+                borderColour = "text";
+                break;
+              case "unreleased":
+                borderColour = "background";
+                break;
+              case "late":
+                borderColour = "text";
+                icon = faBullhorn;
+                break;
+              case "missed":
+                borderColour = "background";
+                icon = faExclamationCircle;
+                break;
+              case "complete":
+                borderColour = "background";
+                icon = faCheckCircle;
+                break;
+            }
+            return (
+              <Row
+                key={e.id}
+                onClick={() => {
+                  setActiveEvent(e);
+                  setShowModal(true);
+                }}
+                style={{
+                  backgroundColor: `var(--${cardColour}-background)`,
+                  color: `var(--${cardColour}-text)`,
+                  borderColor: `var(--${cardColour}-${borderColour})`,
+                }}
+                className={styles.listRow}
+              >
+                <span
+                  className={styles.eventTitle}
+                  style={{
+                    fontSize: "1rem",
+                  }}
+                >
+                  <span className={styles.eventPrefix}>{e.prefix}&nbsp;</span>
+                  {e.title}
+                </span>
+
+                {icon && (
+                  <FontAwesomeIcon className={styles.icon} icon={icon} />
+                )}
+              </Row>
+            );
+          })}
+      </div>
     </>
   );
 };
