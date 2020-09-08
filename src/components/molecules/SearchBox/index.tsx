e03c52eaf2d310844052d41d5575c19c938611c6
danieldeng2 <danieldeng223@gmail.com>
2020-09-08 16:41:31 +0100

    Refactor searchbox to be more general

--- src/components/molecules/SearchBox/index.tsx
+++ src/components/molecules/SearchBox/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styles from "./style.module.scss";
 
 import InputGroup from "react-bootstrap/InputGroup";
@@ -8,19 +8,43 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { titleCase } from "utils/functions";
 
 export interface SearchBoxProps {
   searchText: string;
   onSearchTextChange: (searchText: string) => void;
-  tags?: string[];
+  prompts?: {
+    title: string;
+    list: {
+      name: string;
+      value: string;
+    }[];
+  }[];
 }
 
 const SearchBox: React.FC<SearchBoxProps> = ({
   searchText,
   onSearchTextChange,
-  tags,
+  prompts,
 }: SearchBoxProps) => {
+  let PromptElements: ReactElement[] = [];
+  prompts?.forEach(({ title, list }) => {
+    PromptElements.push(
+      <Dropdown.Header className={styles.dropdownHeader}>
+        {title}:{" "}
+      </Dropdown.Header>
+    );
+    list.forEach(({ name, value }) => {
+      PromptElements.push(
+        <Dropdown.Item
+          className={styles.dropdownItem}
+          onClick={() => onSearchTextChange(`${searchText} ${value} `)}
+        >
+          {name}
+        </Dropdown.Item>
+      );
+    });
+  });
+
   return (
     <Dropdown alignRight>
       <InputGroup>
@@ -37,60 +61,7 @@ const SearchBox: React.FC<SearchBoxProps> = ({
           <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components" />
 
           <Dropdown.Menu alignRight className={styles.dropdownMenu}>
-            <Dropdown.Header className={styles.dropdownHeader}>
-              Types:{" "}
-            </Dropdown.Header>
-            <Dropdown.Item
-              className={styles.dropdownItem}
-              onClick={() => onSearchTextChange(`${searchText} type(pdf) `)}
-            >
-              PDF
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={styles.dropdownItem}
-              onClick={() => onSearchTextChange(`${searchText} type(video) `)}
-            >
-              Video
-            </Dropdown.Item>
-            <Dropdown.Item
-              className={styles.dropdownItem}
-              onClick={() => onSearchTextChange(`${searchText} type(file) `)}
-            >
-              File
-            </Dropdown.Item>
-            <Dropdown.Header className={styles.dropdownHeader}>
-              Tags:{" "}
-            </Dropdown.Header>
-            <Dropdown.Item
-              className={styles.dropdownItem}
-              onClick={() => onSearchTextChange(`${searchText} tag(new) `)}
-            >
-              New
-            </Dropdown.Item>
-						<Dropdown.Item
-              className={styles.dropdownItem}
-              onClick={() => onSearchTextChange(`${searchText} tag(week 1) `)}
-            >
-              Week 1
-            </Dropdown.Item>
-            {tags
-              ? tags
-									.filter((tag) => tag.toLowerCase() !== "new")
-									.filter((tag) => !tag.toLowerCase().startsWith("week"))
-                  .map((tag) => {
-                    return (
-                      <Dropdown.Item
-                        className={styles.dropdownItem}
-                        onClick={() =>
-                          onSearchTextChange(`${searchText} tag(${tag}) `)
-												}
-												key={tag}
-                      >
-                        {titleCase(tag)}
-                      </Dropdown.Item>
-                    );
-                  })
-              : null}
+            {PromptElements}
           </Dropdown.Menu>
         </InputGroup.Append>
       </InputGroup>
--- src/components/pages/ModuleSubmissions/index.tsx
+++ src/components/pages/ModuleSubmissions/index.tsx
@@ -6,6 +6,7 @@ import Row from "react-bootstrap/esm/Row";
 import MyBreadcrumbs from "components/atoms/MyBreadcrumbs";
 import ModuleEventCard from "components/atoms/ModuleEventCard";
 import Col from "react-bootstrap/esm/Col";
+import SearchBox from "components/molecules/SearchBox";
 
 interface Props {
   moduleID: string;
@@ -14,21 +15,23 @@ const ModuleSubmissions: React.FC<Props> = ({ moduleID }) => {
   let [showModal, setShowModal] = useState(false);
   let [activeEvent, setActiveEvent] = useState<TimelineEvent | undefined>(
     undefined
-  );
+	);
+	let [searchText, setSearchText] = useState("");
+	
   let activeDay = new Date("2020-10-12");
   return (
     <>
       <MyBreadcrumbs/>
+			<SearchBox searchText={searchText} onSearchTextChange={setSearchText} />
       <EventModal
         event={activeEvent}
         show={showModal}
         onHide={() => setShowModal(false)}
         activeDay={activeDay}
       />
-      <Row >
+      <Row style={{marginTop: "1.25rem"}}>
         {eventsData
 					.filter(({ moduleCode }) => moduleCode === moduleID)
-          // .filter(({ status }) => status === "due" || status === "late")
           .map((e) => (
             <Col
 							xs={12}
