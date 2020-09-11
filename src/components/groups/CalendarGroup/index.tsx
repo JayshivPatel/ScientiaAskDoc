3406b6ebfdee4f97d0a0452bd8206317b3fa9145
danieldeng2 <danieldeng223@gmail.com>
2020-09-11 18:04:38 +0100

    Sorts calendar events by time

--- src/components/groups/CalendarGroup/index.tsx
+++ src/components/groups/CalendarGroup/index.tsx
@@ -39,7 +39,7 @@ const CalendarGroup: React.FC = () => {
       .then((data) => {
         if (data) {
           let currEvents: CalendarEvent[] = [];
-          data.forEach((element: any) => {
+          data.sort(sortEvents).forEach((element: any) => {
             const timeStart = toShortTimeString(element.start);
             const timeEnd = toShortTimeString(element.end);
             console.log(element.catorgory);
@@ -88,4 +88,17 @@ function toShortTimeString(date: Date) {
     minute: "2-digit",
   });
 }
+
+function sortEvents(e1: any, e2: any) {
+  let e1Start = new Date(e1.start);
+  let e1End = new Date(e1.end);
+  let e2Start = new Date(e2.start);
+  let e2End = new Date(e2.end);
+
+  if (e1Start.getTime() !== e2Start.getTime()) {
+    return e1Start.getTime() - e2Start.getTime();
+  }
+  return e2End.getTime() - e1End.getTime();
+}
+
 export default CalendarGroup;
