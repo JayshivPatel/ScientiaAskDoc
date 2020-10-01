f286192cfa3f9666aa53cf5719d9a167d09dac93
Ivan Procaccini <ivanprocaccini905@gmail.com>
2020-10-01 14:30:29 +0200

    Add 70008 in modules list

--- src/components/pages/ModuleList/list.ts
+++ src/components/pages/ModuleList/list.ts
@@ -1,76 +1,16 @@
-import { ProgressStatus, Module } from "constants/types";
+import { ProgressStatus, Module } from "constants/types"
 
-export const modulesList : Module[] = [
-  {
-    title: "Introduction to Computer Systems",
-    code: "CO112",
-    terms: ["Autumn"],
-    progressStatus: ProgressStatus.IN_PROGRESS,
-    progressPercent: 93,
-    content: "",
-  },
-  {
-    title: "Introduction to Computer Architecture",
-    code: "CO113",
-    terms: ["Spring"],
-    progressStatus: ProgressStatus.NOT_STARTED,
-    progressPercent: 0,
-    content: "",
-  },
-  {
-    title: "Introduction to Databases",
-    code: "CO130",
-    terms: ["Spring"],
-    progressStatus: ProgressStatus.NOT_STARTED,
-    progressPercent: 0,
-    content: "",
-  },
-  {
-    title: "Java",
-    code: "CO120.2",
-    terms: ["Autumn", "Spring", "Summer"],
-    progressStatus: ProgressStatus.IN_PROGRESS,
-    progressPercent: 20,
-    content: "",
-  },
-  {
-    title: "Introduction to Logic",
-    code: "CO140",
-    terms: ["Autumn"],
-    progressStatus: ProgressStatus.IN_PROGRESS,
-    progressPercent: 50,
-    content: "",
-  },
-  {
-    title: "Reasoning About Programs",
-    code: "CO141",
-    terms: ["Spring"],
-    progressStatus: ProgressStatus.NOT_STARTED,
-    progressPercent: 0,
-    content: "",
-  },
-  {
-    title: "Discrete Mathematics",
-    code: "CO142",
-    terms: ["Autumn"],
-    progressStatus: ProgressStatus.IN_PROGRESS,
-    progressPercent: 60,
-    content: "",
-  },
-  {
-    title: "Mathematical Methods",
-    code: "CO145",
-    terms: ["Autumn"],
-    progressStatus: ProgressStatus.IN_PROGRESS,
-    progressPercent: 45,
-    content: "",
-  },
-  {
-    title: "Graphs and Algorithms",
-    code: "CO150",
-    terms: ["Spring"],
-    progressStatus: ProgressStatus.NOT_STARTED,
-    progressPercent: 0,
-    content: "",
-  },
-];
+// This is hardcoded during the Beta-testing phase.
+// Once the proxy API for DBC will be up and running,
+// we will retrieve these information from that.
+
+export const modulesList: Module[] = [
+	{
+		title: "Concurrent Processes",
+		code: "70008",
+		terms: ["Autumn"],
+		progressStatus: ProgressStatus.IN_PROGRESS,
+		progressPercent: 0,
+		content: "",
+	},
+]
