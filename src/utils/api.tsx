a03513fd29834c60ecf03233c8ec600290660e1f
Ivan Procaccini <ivanprocaccini905@gmail.com>
2020-10-01 18:42:12 +0200

    Comment out unnecessary ACAO header

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,18 +1,18 @@
-import authConstants from "constants/auth";
+import authConstants from "constants/auth"
 import { methods } from "constants/routes"
 
 interface RequestOptions {
-    [key: string]: any
+	[key: string]: any
 }
 
 export interface RequestData {
-  url: string,
-  method: string,
-  onSuccess: any,
-  onError: (message: string) => void,
-  body?: any,
-  sendFile?: boolean,
-  returnBlob?: boolean
+	url: string
+	method: string
+	onSuccess: any
+	onError: (message: string) => void
+	body?: any
+	sendFile?: boolean
+	returnBlob?: boolean
 }
 
 // API calling interface. onSuccess and onError are functions that take in data
@@ -20,76 +20,76 @@ export interface RequestData {
 // method is GET
 // Note: will trigger CORS OPTIONS preflight due to the Authorization header
 export async function request(data: RequestData) {
-  let headers: { [key: string]: string } = {
-    "Authorization": authConstants.ACCESS_TOKEN_HEADER(),
-    "Access-Control-Allow-Origin": "*",
-  };
+	let headers: { [key: string]: string } = {
+		Authorization: authConstants.ACCESS_TOKEN_HEADER(),
+		// "Access-Control-Allow-Origin": "*", THIS SHOULD NOT BE NEEDED HERE
+	}
 
-  if (!data.sendFile) {
-    headers["Content-Type"] = "application/json";
-  }
+	if (!data.sendFile) {
+		headers["Content-Type"] = "application/json"
+	}
 
-  var options: RequestOptions = {
-    method: data.method,
-    mode: "cors",
-    headers: headers
-  };
+	var options: RequestOptions = {
+		method: data.method,
+		mode: "cors",
+		headers: headers,
+	}
 
-  if (data.method === methods.GET || data.method === methods.DELETE) {
-    data.url = data.url + "?" + new URLSearchParams(data.body);
-  } else {
-    options.body = data.sendFile ? data.body : JSON.stringify(data.body);
-  }
+	if (data.method === methods.GET || data.method === methods.DELETE) {
+		data.url = data.url + "?" + new URLSearchParams(data.body)
+	} else {
+		options.body = data.sendFile ? data.body : JSON.stringify(data.body)
+	}
 
-  return fetch(data.url, options)
-    .then(response => {
-      if (!response.ok) {
-        throw response;
-      }
-      if (data.returnBlob) {
-        return response.blob();
-      }
-      return response.json();
-    })
-    .then(responseData => {
-      data.onSuccess(responseData);
-    })
-    .catch(error => {
-      // Parse error object if returned by API
-      // Currently follows Materials API error shape
-      try {
-        error.json().then((body: { message: string; }) => {
-          data.onError(body.message);
-        });
-      } catch (e) {
-        data.onError(error);
-      }
-    })
+	return fetch(data.url, options)
+		.then((response) => {
+			if (!response.ok) {
+				throw response
+			}
+			if (data.returnBlob) {
+				return response.blob()
+			}
+			return response.json()
+		})
+		.then((responseData) => {
+			data.onSuccess(responseData)
+		})
+		.catch((error) => {
+			// Parse error object if returned by API
+			// Currently follows Materials API error shape
+			try {
+				error.json().then((body: { message: string }) => {
+					data.onError(body.message)
+				})
+			} catch (e) {
+				data.onError(error)
+			}
+		})
 }
 
 // Utility that downloads files fetched by request (assumes GET)
 export async function download(url: string, filename: string, body?: any) {
-  const onSuccess = (blob: any) => {
-    // TODO: Try to navigate straight to the endpoint url instead of creating an object url
-    let url = URL.createObjectURL(blob);
-    let a = document.createElement("a");
-    a.href = url;
-    a.download = filename;
-    a.click();
-    a.remove();
-  };
+	const onSuccess = (blob: any) => {
+		// TODO: Try to navigate straight to the endpoint url instead of creating an object url
+		let url = URL.createObjectURL(blob)
+		let a = document.createElement("a")
+		a.href = url
+		a.download = filename
+		a.click()
+		a.remove()
+	}
 
-  const onError = (message: string) => {
-    // TODO: Deal with download failure (also with openResource)
-    console.log(message);
-  };
+	const onError = (message: string) => {
+		// TODO: Deal with download failure (also with openResource)
+		console.log(message)
+	}
 
-  request({
-    url: url,
-    method: methods.GET,
-    onSuccess,
-    onError,
-    returnBlob: true,
-    body
-  });
+	request({
+		url: url,
+		method: methods.GET,
+		onSuccess,
+		onError,
+		returnBlob: true,
+		body,
+	})
 }
