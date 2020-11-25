2e6d9bebb5bcc55092f4bcb3c37ed8eb0345cde4
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-25 23:46:37 +0000

    Tests for ModuleCard

---
+++ src/components/cards/ModuleCard/ModuleCard.test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { Module, ProgressStatus } from "../../../constants/types";
+import ModuleCard from "./index";
+import Card from "react-bootstrap/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCanadianMapleLeaf } from "@fortawesome/free-brands-svg-icons";
+import { BrowserRouter as Router } from "react-router-dom";
+
+configure({ adapter: new Adapter() });
+
+describe("<ModuleCard />", () => {
+  const module: Module = {
+    title: "Test Title",
+    code: "CO60050",
+    terms: ["Autumn"],
+    progressStatus: ProgressStatus.IN_PROGRESS,
+    progressPercent: 50,
+    content: "Test Content",
+    subscriptionLevel: 1,
+  };
+  const wrapper = mount(
+    <Router>
+      <ModuleCard module={module} />
+    </Router>
+  );
+  it("Check title", () => {
+    expect(wrapper.find(Card.Title).text()).toBe(module.title);
+  });
+  it("Check term", () => {
+    expect(wrapper.find(FontAwesomeIcon).prop("icon")).toBe(
+      faCanadianMapleLeaf
+    );
+  });
+  it("Check flag hasMaterials", () => {
+    expect(wrapper.find(Card).prop("border")).toBe("danger");
+  });
+});
