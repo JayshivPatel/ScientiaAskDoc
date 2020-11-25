e0d07898f01d53d50fef880e6fb527ce57053581
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-25 16:52:51 +0000

    Reformat GroupMemberCard Prettier

--- src/components/cards/GroupMemberCard/index.tsx
+++ src/components/cards/GroupMemberCard/index.tsx
@@ -1,28 +1,25 @@
-import styles from "./style.module.scss"
+import styles from "./style.module.scss";
 import React from "react";
 import classNames from "classnames";
-import Image from "react-bootstrap/Image"
-import {GroupFormationMemberInfo} from "../../../constants/types";
-import Container from "react-bootstrap/Container"
-import {theme} from "../../../utils/functions";
-import moment from "moment";
+import Image from "react-bootstrap/Image";
+import { GroupFormationMemberInfo } from "../../../constants/types";
+import Container from "react-bootstrap/Container";
+import { theme } from "../../../utils/functions";
 import Badge from "react-bootstrap/Badge";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {
-  faTimes,
-} from "@fortawesome/free-solid-svg-icons"
-import {Role} from "../../sections/SubmissionGroupFormation";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Role } from "../../sections/SubmissionGroupFormation";
 
 interface ExtraProps {
-  currentUser: string
-  currentRole: string
-  onRemoveButtonClick?: (username: string) => void
-  onClick?: (mouseEvent: React.MouseEvent) => void
+  currentUser: string;
+  currentRole: string;
+  onRemoveButtonClick?: (username: string) => void;
+  onClick?: (mouseEvent: React.MouseEvent) => void;
 }
 
-type Props = GroupFormationMemberInfo & ExtraProps
+type Props = GroupFormationMemberInfo & ExtraProps;
 
 const GroupMemberCard: React.FC<Props> = ({
   currentUser,
@@ -37,60 +34,55 @@ const GroupMemberCard: React.FC<Props> = ({
 }) => {
   const [tagStyle, tagText] = (() => {
     if (signatureTime) {
-      return [styles.tagTeal, `SIGNED`]
+      return [styles.tagTeal, `SIGNED`];
     } else if (currentUser === username && currentRole === Role.MEMBER) {
-      return [styles.tagPink, "Click Here to Sign"]
+      return [styles.tagPink, "Click Here to Sign"];
     } else {
-      return [styles.tagPink, "Not Signed"]
+      return [styles.tagPink, "Not Signed"];
     }
-  })()
+  })();
 
   return (
-    <Col style={{padding: "0.5em", zIndex: 0}}>
+    <Col style={{ padding: "0.5em", zIndex: 0 }}>
       <Container
-        className={classNames(styles.groupMemberCard, role === 'leader' && styles.leaderCard)}
+        className={classNames(
+          styles.groupMemberCard,
+          role === "leader" && styles.leaderCard
+        )}
         onClick={onClick}
       >
         <div
-          style={{ verticalAlign: 'top', height: '100%', paddingTop: '0.5rem' }}
+          style={{ verticalAlign: "top", height: "100%", paddingTop: "0.5rem" }}
         >
           <Image
             src={`/images/${theme()}/user.png`}
             className={styles.profilePic}
           />
         </div>
-        {onRemoveButtonClick &&
-        (<Button
-          className={styles.removeButton}
-          variant={"secondary"}
-          onClick={(event) => {
-            event.stopPropagation()
-            onRemoveButtonClick(username)
-          }}
-        >
-          <FontAwesomeIcon icon={faTimes}/>
-        </Button>)}
+        {onRemoveButtonClick && (
+          <Button
+            className={styles.removeButton}
+            variant={"secondary"}
+            onClick={(event) => {
+              event.stopPropagation();
+              onRemoveButtonClick(username);
+            }}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </Button>
+        )}
         <div className={styles.memberInfoSection}>
-          <h4 className={styles.memberName}>
-            {realName}
-          </h4>
-          <div style={{ display: 'flex' }}>
+          <h4 className={styles.memberName}>{realName}</h4>
+          <div style={{ display: "flex" }}>
             <h6 className={styles.memberName}>{username}</h6>
             <p className={styles.memberClass}>{classEnrolled}</p>
           </div>
-          <Badge
-            pill
-            key={"sign"}
-            className={classNames(
-              styles.tag,
-              tagStyle
-            )}
-          >
+          <Badge pill key={"sign"} className={classNames(styles.tag, tagStyle)}>
             {tagText}
           </Badge>
         </div>
       </Container>
     </Col>
-)
-}
-export default GroupMemberCard
+  );
+};
+export default GroupMemberCard;
