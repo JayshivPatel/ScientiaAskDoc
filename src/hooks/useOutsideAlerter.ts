872785038edc3a54bbab13125328d1d9690896d8
FrankYu <zhige.yu18@imperial.ac.uk>
2020-11-25 16:47:16 +0800

    Move useOutsideAlerter.ts into src/hooks

--- src/hooks/useOutsideAlerter.ts
+++ src/hooks/useOutsideAlerter.ts
@@ -1,5 +1,4 @@
-import React, { useEffect, RefObject } from "react";
-
+import { useEffect, RefObject } from "react";
 
 export default function useOutsideAlerter(ref: RefObject<HTMLDivElement>, callback: () => void) {
   useEffect(() => {
