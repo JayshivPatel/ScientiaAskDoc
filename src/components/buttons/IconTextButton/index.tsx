6d8c228d19922a3c4ae16fbf69ff800e584a963e
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-25 02:07:38 +0000

    Reformat IconTextButton Prettier

--- src/components/buttons/IconTextButton/index.tsx
+++ src/components/buttons/IconTextButton/index.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import styles from "./style.module.scss"
-import classNames from "classnames"
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
-import OverlayTrigger from "react-bootstrap/OverlayTrigger"
-import Tooltip from "react-bootstrap/Tooltip"
-import Button from "react-bootstrap/Button"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react";
+import styles from "./style.module.scss";
+import classNames from "classnames";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Props {
-  icon: IconDefinition,
-  text: string 
-  tooltip?: string
-  onClick?: (e: MouseEvent) => void
-  buttonProps?: any
+  icon: IconDefinition;
+  text: string;
+  tooltip?: string;
+  onClick?: (e: MouseEvent) => void;
+  buttonProps?: any;
 }
 
 const IconTextButton: React.FC<Props> = ({
@@ -22,22 +22,28 @@ const IconTextButton: React.FC<Props> = ({
   onClick,
   buttonProps,
 }) => {
-
   return (
     <OverlayTrigger
-      overlay={<Tooltip id={`tooltip-${tooltip}`} style={{ zIndex: 99999 }}>{tooltip}</Tooltip>}>
+      overlay={
+        <Tooltip id={`tooltip-${tooltip}`} style={{ zIndex: 99999 }}>
+          {tooltip}
+        </Tooltip>
+      }
+    >
       <Button
         {...buttonProps}
         variant="secondary"
-        className={classNames(styles.sectionHeaderButton, styles.iconTextButton)}
+        className={classNames(
+          styles.sectionHeaderButton,
+          styles.iconTextButton
+        )}
         onClick={onClick}
       >
         <FontAwesomeIcon icon={icon} />
         {text}
-
       </Button>
     </OverlayTrigger>
-  )
-}
+  );
+};
 
-export default IconTextButton
+export default IconTextButton;
