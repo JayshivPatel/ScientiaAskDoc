40ac088be2a13e7293a790959354a7311ccb2246
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-27 03:32:10 +0000

    Set up tests for CategoryHeader

---
+++ src/components/headings/CategoryHeader/categoryHeader.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { mount } from "enzyme";
+import { faFileCode } from "@fortawesome/free-solid-svg-icons";
+import CategoryHeader from "./index";
+
+describe("<Category Header", () => {
+  const heading = "Test Heading";
+  const onSelectAllClick = jest.fn();
+  const selectAllIcon = faFileCode;
+  const wrapper = mount(
+    <CategoryHeader
+      heading={heading}
+      onSelectAllClick={onSelectAllClick}
+      selectAllIcon={selectAllIcon}
+    />
+  );
+});
