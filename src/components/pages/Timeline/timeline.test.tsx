4654bf7d2c1cb6a2ac65c629cca82e96de372f64
ss16118 <siyuan.shen18@imperial.ac.uk>
2020-11-27 17:13:38 +0800

    Added timeline test

--- src/components/pages/Timeline/timeline.test.tsx
+++ src/components/pages/Timeline/timeline.test.tsx
@@ -3,6 +3,7 @@ import {mount, shallow} from "enzyme";
 import Timeline from "./index";
 import {Module, ProgressStatus} from "../../../constants/types";
 import TermSwitcher from "./components/TermSwitcher";
+import ModuleRows from "./components/ModuleRows";
 
 describe("</TimeLine>", () => {
   const modules: Module[] = [
@@ -16,21 +17,37 @@ describe("</TimeLine>", () => {
       subscriptionLevel: 1,
       terms: ["Autumn"],
       title: "Simulation and Modelling",
-    }
+    },
+    {
+      canManage: false,
+      code: "60021",
+      content: "",
+      hasMaterials: false,
+      progressPercent: 7,
+      progressStatus: ProgressStatus.IN_PROGRESS,
+      subscriptionLevel: 1,
+      terms: ["Autumn"],
+      title: "Software Engineering",
+    },
   ]
 
+  const wrapper = shallow(
+    <Timeline
+      initSideBar={jest.fn()}
+      revertSideBar={jest.fn()}
+      term={"Autumn"}
+      setTerm={jest.fn()}
+      onEventClick={jest.fn()}
+      modules={modules}
+      timelineEvents={{}}
+      modulesTracks={{}}
+    />)
+
   it("Able to render the Timeline", () => {
-    const wrapper = shallow(
-      <Timeline
-        initSideBar={jest.fn()}
-        revertSideBar={jest.fn()}
-        term={"Autumn"}
-        setTerm={jest.fn()}
-        onEventClick={jest.fn()}
-        modules={modules}
-        timelineEvents={{}}
-        modulesTracks={{}}
-      />)
       expect(wrapper.find(TermSwitcher).prop("term")).toBe("Autumn")
+  })
+
+  it("Able to display the modules", () => {
+    expect(wrapper.find(ModuleRows).prop("modulesList")).toHaveLength(2)
   })
 })
