856dcf4b8b8a45678c65466680a4de07b5a68f16
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-27 00:45:46 +0000

    Fix SideBarOutlineGroup Tests

--- src/components/groups/SideBarOutlineGroup/sideBarOutlineGroup.test.tsx
+++ src/components/groups/SideBarOutlineGroup/sideBarOutlineGroup.test.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { mount } from "enzyme";
-import SideBarTabGroup from "../SideBarTabGroup";
 import SideBarOutlineGroup from "./index";
+import { BrowserRouter } from "react-router-dom";
+import SideBarTabGroup from "../SideBarTabGroup";
 
 describe("<SideBarOutlineGroup />", () => {
-  const wrapper = mount(<SideBarOutlineGroup />);
+  const wrapper = mount(
+    <BrowserRouter>
+      <SideBarOutlineGroup />
+    </BrowserRouter>
+  );
   it("Check all", () => {
-    // expect(wrapper.find(SideBarTabGroup)).toBeDefined();
-    // expect(wrapper.find(SideBarTabGroup).prop("title")).toBe("Outline");
-    // expect(wrapper.find(SideBarTabGroup).prop("buttons").length).toBe(5);
+    expect(wrapper.find(SideBarTabGroup)).toBeDefined();
+    expect(wrapper.find(SideBarTabGroup).prop("title")).toBe("Outline");
+    expect(wrapper.find(SideBarTabGroup).prop("buttons").length).toBe(5);
   });
 });
