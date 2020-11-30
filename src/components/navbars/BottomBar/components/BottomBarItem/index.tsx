1e59ea62ef3500906f6f2f59c82356b3f9b260a5
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-30 13:18:47 +0000

    Reformat BottomBarItem Prettier

--- src/components/navbars/BottomBar/components/BottomBarItem/index.tsx
+++ src/components/navbars/BottomBar/components/BottomBarItem/index.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import Button from "react-bootstrap/Button"
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { NavLink } from "react-router-dom"
-import styles from "./style.module.scss"
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import styles from "./style.module.scss";
 
 export interface BottomBarItemProps {
   page: {
-    name: string
-    path: string
-    icon: IconDefinition
-  }
+    name: string;
+    path: string;
+    icon: IconDefinition;
+  };
 }
 
 const BottomBarItem: React.FC<BottomBarItemProps> = ({
@@ -23,12 +23,13 @@ const BottomBarItem: React.FC<BottomBarItemProps> = ({
       to={page.path}
       className={styles.btn}
       id={"bottom-" + page.name}
-      variant="secondary">
+      variant="secondary"
+    >
       <div className="button-holder">
         <FontAwesomeIcon icon={page.icon} size="lg" />
       </div>
     </Button>
-  )
-}
+  );
+};
 
-export default BottomBarItem
+export default BottomBarItem;
