fb37e0c76c5407d708fce235096336e1a01ac9cd
HY <yan@dyn3168-251.wlan.ic.ac.uk>
2020-11-30 13:28:20 +0000

    Reformat BottomBar Prettier

--- src/components/navbars/BottomBar/index.tsx
+++ src/components/navbars/BottomBar/index.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import Navbar from "react-bootstrap/Navbar"
-import ButtonGroup from "react-bootstrap/ButtonGroup"
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
-import BottomBarItem from "components/navbars/BottomBar/components/BottomBarItem"
-import styles from "./style.module.scss"
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import BottomBarItem from "components/navbars/BottomBar/components/BottomBarItem";
+import styles from "./style.module.scss";
 
 export interface BottomBarProps {
   pages: {
-    name: string
-    path: string
-    icon: IconDefinition
-  }[]
+    name: string;
+    path: string;
+    icon: IconDefinition;
+  }[];
 }
 
 const BottomBar: React.FC<BottomBarProps> = ({ pages }: BottomBarProps) => {
@@ -18,13 +18,14 @@ const BottomBar: React.FC<BottomBarProps> = ({ pages }: BottomBarProps) => {
     <Navbar className={"footer " + styles.bottomBar}>
       <ButtonGroup
         aria-label="Basic example"
-        className={styles.bottomBarButtons}>
+        className={styles.bottomBarButtons}
+      >
         {pages.map((page) => (
           <BottomBarItem page={page} key={page.name} />
         ))}
       </ButtonGroup>
     </Navbar>
-  )
-}
+  );
+};
 
-export default BottomBar
+export default BottomBar;
