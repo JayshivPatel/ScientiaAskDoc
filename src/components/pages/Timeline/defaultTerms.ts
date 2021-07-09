a5185b8b2f1d10565ac1f01c848c97ca11d4b369
Ivan Procaccini <ivanprocaccini905@gmail.com>
2021-07-09 10:39:14 +0100

    Fix month offset to follow quirks of JS Date

--- src/components/pages/Timeline/defaultTerms.ts
+++ src/components/pages/Timeline/defaultTerms.ts
@@ -3,61 +3,64 @@ import { Term } from "../../../constants/types";
 /**
  * Convenience function to provide a reasonable default value for
  * the current term while waiting for actual year term details.
+ * It so appears that the MONTH attribute in JS Date() starts counting from
+ * 0 (for Jan): this flaw is inherited from JAVA 1.0.
+ * https://stackoverflow.com/questions/2552483/why-does-the-month-argument-range-from-0-to-11-in-javascripts-date-constructor
  */
 function getDefaultTerm(): Term {
   const today = new Date();
   let month = today.getMonth();
   let year = today.getFullYear();
   let day = today.getDate();
-  if (month >= 10) {
-    if (month === 12 && day >= 24)
+  if (month >= 9) {
+    if (month === 11 && day >= 24)
       return {
         label: "Christmas",
         weeks: 3,
-        start: new Date(year, 12, 24),
-        end: new Date(year + 1, 1, 7),
+        start: new Date(year, 11, 24),
+        end: new Date(year + 1, 0, 7),
       };
     return {
       label: "Autumn",
-      start: new Date(year, 10, 1),
-      end: new Date(year, 12, 31),
+      start: new Date(year, 9, 1),
+      end: new Date(year, 11, 31),
       weeks: 11,
     };
   }
-  if (month >= 1 && month <= 3) {
-    if (month === 1 && day <= 6)
+  if (month >= 0 && month <= 2) {
+    if (month === 0 && day <= 6)
       return {
         label: "Christmas",
         weeks: 3,
-        start: new Date(year - 1, 12, 24),
-        end: new Date(year, 1, 6),
+        start: new Date(year - 1, 11, 24),
+        end: new Date(year, 0, 6),
       };
     return {
       label: "Spring",
-      start: new Date(year, 1, 1),
-      end: new Date(year, 3, 31),
+      start: new Date(year, 0, 1),
+      end: new Date(year, 2, 31),
       weeks: 11,
     };
   }
-  if (month >= 4 && month <= 6) {
-    if (month === 4 && day <= 20)
+  if (month >= 3 && month <= 5) {
+    if (month === 3 && day <= 20)
       return {
         label: "Easter",
         weeks: 5,
-        start: new Date(year, 4, 1),
-        end: new Date(year, 4, 20),
+        start: new Date(year, 3, 1),
+        end: new Date(year, 3, 20),
       };
     return {
       label: "Summer",
-      start: new Date(year, 4, 20),
-      end: new Date(year, 6, 30),
+      start: new Date(year, 3, 20),
+      end: new Date(year, 5, 30),
       weeks: 9,
     };
   }
   return {
     label: "June-Sept",
-    start: new Date(year, 7, 1),
-    end: new Date(year, 9, 30),
+    start: new Date(year, 6, 1),
+    end: new Date(year, 8, 30),
     weeks: 12,
   };
 }
