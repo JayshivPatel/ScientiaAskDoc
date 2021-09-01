18bad2ef606222382050d7837393bc9cc6dd432b
Ivan Procaccini <ivanprocaccini905@gmail.com>
2021-09-01 12:26:01 +0100

    Refactor file cards view into separate component and add tests for it

---
+++ src/components/pages/modulePages/ModuleFeedbackResources/components/FileCardsView.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import Row from "react-bootstrap/esm/Row"
+import Col from "react-bootstrap/esm/Col"
+import { faFile } from "@fortawesome/free-solid-svg-icons"
+import WarningJumbotron from "../../../../suspense/WarningJumbotron"
+import { Feedback } from "../../../../../constants/types"
+import FileCard from "../../../../cards/FileCard"
+import history from "../../../../../history"
+
+interface FileCardsViewProps {
+  feedbackItems: Feedback[]
+}
+
+const navigateToFeedback = (feedback: Feedback) => {
+  history.push(`/modules/${feedback.course}/feedback/${feedback.exercise}`)
+}
+
+const FileCardsView: React.FC<FileCardsViewProps> = ({
+  feedbackItems,
+}: FileCardsViewProps) => {
+  if (feedbackItems.length === 0) {
+    return (
+      <WarningJumbotron message="No feedback has been released for this course yet." />
+    )
+  }
+  return (
+    <div style={{ paddingTop: "0.75rem" }}>
+      <Row style={{ marginRight: "-0.625rem", marginLeft: "-0.625rem" }}>
+        {feedbackItems
+          // Show feedback from most to least recent
+          .sort((f1, f2) => f2.id - f1.id)
+          .map((feedback: Feedback) => (
+            <Col
+              xs={12}
+              sm={6}
+              md={6}
+              lg={4}
+              xl={3}
+              key={feedback.id}
+              style={{ paddingLeft: "0.625rem", paddingRight: "0.625rem" }}
+            >
+              <FileCard
+                title={feedback.exercise_title}
+                type={"pdf"}
+                icon={faFile}
+                onClick={() => navigateToFeedback(feedback)}
+              />
+            </Col>
+          ))}
+      </Row>
+    </div>
+  )
+}
+export default FileCardsView
--- src/components/pages/modulePages/ModuleFeedbackResources/index.tsx
+++ src/components/pages/modulePages/ModuleFeedbackResources/index.tsx
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from "react"
-import Row from "react-bootstrap/esm/Row"
-import Col from "react-bootstrap/esm/Col"
-import { faFile } from "@fortawesome/free-solid-svg-icons"
 import { Feedback } from "../../../../constants/types"
 import { request } from "../../../../utils/api"
 import { api, methods } from "../../../../constants/routes"
-import WarningJumbotron from "../../../suspense/WarningJumbotron"
 import Dandruff from "../../../headings/Dandruff"
 import LoadingScreen from "../../../suspense/LoadingScreen"
-import FileCard from "../../../cards/FileCard"
-import history from "../../../../history"
+import FileCardsView from "./components/FileCardsView"
 
 interface ModuleFeedbackProps {
   year: string
   moduleTitle: string
   moduleID: string
-}
-
-const navigateToFeedback = (feedback: Feedback) => {
-  history.push(`/modules/${feedback.course}/feedback/${feedback.exercise}`)
 }
 
 const ModuleFeedbackResources: React.FC<ModuleFeedbackProps> = ({
@@ -47,38 +38,7 @@ const ModuleFeedbackResources: React.FC<ModuleFeedbackProps> = ({
   }, [year, moduleID])
 
   const view = () => {
-    if (feedbackItems.length === 0) {
-      return (
-        <WarningJumbotron message="No feedback has been released for this course yet." />
-      )
-    }
-    return (
-      <div style={{ paddingTop: "0.75rem" }}>
-        <Row style={{ marginRight: "-0.625rem", marginLeft: "-0.625rem" }}>
-          {feedbackItems
-            // Show feedback from most to least recent
-            .sort((f1, f2) => f2.id - f1.id)
-            .map((feedback: Feedback) => (
-              <Col
-                xs={12}
-                sm={6}
-                md={6}
-                lg={4}
-                xl={3}
-                key={feedback.id}
-                style={{ paddingLeft: "0.625rem", paddingRight: "0.625rem" }}
-              >
-                <FileCard
-                  title={feedback.exercise_title}
-                  type={"pdf"}
-                  icon={faFile}
-                  onClick={() => navigateToFeedback(feedback)}
-                />
-              </Col>
-            ))}
-        </Row>
-      </div>
-    )
+    return <FileCardsView feedbackItems={feedbackItems} />
   }
 
   return (
