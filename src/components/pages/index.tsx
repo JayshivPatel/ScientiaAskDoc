4849e31792fffb760a240fcc653bf4455b94eef5
Ivan Procaccini <ivanprocaccini905@gmail.com>
2021-09-10 11:40:45 +0100

    Extract YearSubRouter into separate module

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,38 +1,16 @@
-import React, { Suspense, useEffect, useState } from "react"
+import React, { Suspense } from "react"
 import Container from "react-bootstrap/esm/Container"
-import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom"
+import { Redirect, Route, Switch } from "react-router-dom"
 import "./style.scss"
 import classNames from "classnames"
-import { CalendarEvent, Module, TimelineEvent } from "constants/types"
+import { CalendarEvent, TimelineEvent } from "constants/types"
 import LoadingScreen from "components/suspense/LoadingScreen"
 import RightBar from "components/navbars/RightBar"
 import LeftBar from "components/navbars/LeftBar"
-import { request } from "../../utils/api"
-import { api, methods } from "../../constants/routes"
-import { YEAR_OF_NEW_CODES } from "../../constants/doc"
-import ModulesSubRouter from "./subrouters/ModulesSubRouter"
-import { TimelineSubRoute } from "./subrouters/TimelineSubRoute"
 
-const Timeline = React.lazy(() => import("components/pages/Timeline"))
-const ModuleDashboard = React.lazy(
-  () => import("components/pages/modulePages/ModuleDashboard")
-)
+import YearSubRouter from "./subrouters/YearSubRouter"
+
 const Dashboard = React.lazy(() => import("components/pages/Dashboard"))
-const ModuleResources = React.lazy(
-  () => import("./modulePages/ModuleResources")
-)
-const ModuleResource = React.lazy(() => import("./modulePages/ModuleResource"))
-const ModuleFeedbackResources = React.lazy(
-  () => import("./modulePages/ModuleFeedbackResources")
-)
-const ModuleFeedbackResource = React.lazy(
-  () => import("./modulePages/ModuleFeedbackResource")
-)
-const ExamGrading = React.lazy(() => import("./exams/Grading"))
-const ExamPastPapers = React.lazy(() => import("./exams/PastPapers"))
-const ModuleSubmissions = React.lazy(
-  () => import("./modulePages/ModuleSubmissions")
-)
 
 interface StandardViewProps {
   toggledLeft: boolean
@@ -44,67 +22,6 @@ interface StandardViewProps {
   onEventClick: (e?: TimelineEvent) => void
   onCalendarClick: (e?: CalendarEvent) => void
   year: string
-}
-
-interface YearDependentRoutesProps {
-  year: string
-  timelineConfig: {
-    onEventClick: (e?: TimelineEvent) => void
-    initSideBar: () => void
-    revertSideBar: () => void
-  }
-}
-
-const YearDependentSubRoutes: React.FC<YearDependentRoutesProps> = ({
-  year,
-  timelineConfig,
-}: YearDependentRoutesProps) => {
-  let { path, url } = useRouteMatch()
-  const [modules, setModules] = useState<Module[]>([])
-  useEffect(() => {
-    const onSuccess = (modules: Module[]) => {
-      setModules(
-        modules.map((module) => ({
-          title: module.title,
-          code: year < YEAR_OF_NEW_CODES ? `CO${module.code}` : module.code,
-          can_manage: module.can_manage,
-          has_materials: module.has_materials,
-          // Hardcoded stuff, we don't have this data currently
-          terms: ["Autumn", "Spring", "Summer"],
-          content: "",
-          subscriptionLevel: (Math.floor(Math.random() * 3) + 1) as 1 | 2 | 3,
-        }))
-      )
-    }
-
-    request({
-      endpoint: api.MATERIALS_COURSES(year),
-      method: methods.GET,
-      onSuccess: onSuccess,
-      onError: (message) => console.log(`Failed to obtain modules: ${message}`),
-    })
-  }, [year])
-
-  return (
-    <Switch>
-      <Route path={`${path}/modules`}>
-        <ModulesSubRouter year={year} modules={modules} />
-      </Route>
-
-      <Route
-        path={`${path}/timeline`}
-        render={(props) => {
-          return (
-            <TimelineSubRoute
-              timelineConfig={timelineConfig}
-              modules={modules}
-              year={props.match.params.year}
-            />
-          )
-        }}
-      />
-    </Switch>
-  )
 }
 
 const StandardView: React.FC<StandardViewProps> = ({
@@ -158,7 +75,7 @@ const StandardView: React.FC<StandardViewProps> = ({
             path="/:year"
             render={(props) => {
               return (
-                <YearDependentSubRoutes
+                <YearSubRouter
                   year={props.match.params.year}
                   timelineConfig={timelineConfig}
                 />
---
+++ src/components/pages/subrouters/YearSubRouter.tsx
@@ -0,0 +1,71 @@
+import { Module, TimelineEvent } from "../../../constants/types"
+import { YEAR_OF_NEW_CODES } from "../../../constants/doc"
+import { request } from "../../../utils/api"
+import { api, methods } from "../../../constants/routes"
+import ModulesSubRouter from "./ModulesSubRouter"
+import { TimelineSubRoute } from "./TimelineSubRoute"
+import React, { useEffect, useState } from "react"
+import { Route, Switch, useRouteMatch } from "react-router-dom"
+
+interface YearSubRouter {
+  year: string
+  timelineConfig: {
+    onEventClick: (e?: TimelineEvent) => void
+    initSideBar: () => void
+    revertSideBar: () => void
+  }
+}
+
+const YearSubRouter: React.FC<YearSubRouter> = ({
+  year,
+  timelineConfig,
+}: YearSubRouter) => {
+  let { path, url } = useRouteMatch()
+  const [modules, setModules] = useState<Module[]>([])
+  useEffect(() => {
+    const onSuccess = (modules: Module[]) => {
+      setModules(
+        modules.map((module) => ({
+          title: module.title,
+          code: year < YEAR_OF_NEW_CODES ? `CO${module.code}` : module.code,
+          can_manage: module.can_manage,
+          has_materials: module.has_materials,
+          // Hardcoded stuff, we don't have this data currently
+          terms: ["Autumn", "Spring", "Summer"],
+          content: "",
+          subscriptionLevel: (Math.floor(Math.random() * 3) + 1) as 1 | 2 | 3,
+        }))
+      )
+    }
+
+    request({
+      endpoint: api.MATERIALS_COURSES(year),
+      method: methods.GET,
+      onSuccess: onSuccess,
+      onError: (message) => console.log(`Failed to obtain modules: ${message}`),
+    })
+  }, [year])
+
+  return (
+    <Switch>
+      <Route path={`${path}/modules`}>
+        <ModulesSubRouter year={year} modules={modules} />
+      </Route>
+
+      <Route
+        path={`${path}/timeline`}
+        render={(props) => {
+          return (
+            <TimelineSubRoute
+              timelineConfig={timelineConfig}
+              modules={modules}
+              year={props.match.params.year}
+            />
+          )
+        }}
+      />
+    </Switch>
+  )
+}
+
+export default YearSubRouter
