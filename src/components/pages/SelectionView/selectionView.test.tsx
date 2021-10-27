df3ffaedb36c04f96f91337f5bbcce64380eef03
Fawaz Shah <fawaz010@gmail.com>
2021-10-27 15:14:01 +0100

    Add check on selectionItems to SelectionView tests, add test checking registering selected items

--- src/components/pages/SelectionView/selectionView.test.tsx
+++ src/components/pages/SelectionView/selectionView.test.tsx
@@ -1,19 +1,75 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { mount } from "enzyme"
-import SelectionView from "./index"
+import SelectionView, { SelectionItem, SelectionProps } from "./index"
+
+const DummyComponent: React.FC<{ select: SelectionProps }> = ({ select }) => {
+  return <div />
+}
+
+const DummyComponentClickFirst: React.FC<{ select: SelectionProps }> = ({
+  select,
+}) => {
+  useEffect(() => {
+    select.handleSelectIconClick(1)
+  }, [])
+  return <div />
+}
+
+const selectionItems: SelectionItem[] = [
+  {
+    title: "Propositional Logic Slides",
+    id: 1,
+    type: "pdf",
+  },
+  {
+    title: "First Order Logic Slides",
+    id: 2,
+    type: "pdf",
+  },
+  {
+    title: "Link to Advanced Material",
+    id: 3,
+    type: "link",
+  },
+]
 
 describe("<SelectionView />", () => {
   const wrapper = mount(
     <SelectionView
-      selectionItems={[]}
-      render={jest.fn()}
+      selectionItems={selectionItems}
+      render={jest.fn((select: SelectionProps) => (
+        <DummyComponent select={select} />
+      ))}
       heading="Resources"
       onDownloadClick={(identifiers: number[]) => {}}
       onItemClick={(identifier: number) => {}}
     />
   )
 
-  it("calls the render prop", () => {
+  it("calls the render prop with the correct selection items", () => {
     expect(wrapper.find(SelectionView).prop("render")).toBeCalled()
+    expect(wrapper.find(DummyComponent).prop("select").selectionItems).toEqual(
+      selectionItems
+    )
+  })
+})
+
+describe("<SelectionView />", () => {
+  const wrapper = mount(
+    <SelectionView
+      selectionItems={selectionItems}
+      render={jest.fn((select: SelectionProps) => (
+        <DummyComponentClickFirst select={select} />
+      ))}
+      heading="Resources"
+      onDownloadClick={(identifiers: number[]) => {}}
+      onItemClick={(identifier: number) => {}}
+    />
+  )
+
+  it("registers selected items", () => {
+    expect(
+      wrapper.find(DummyComponentClickFirst).prop("select").isAnySelected()
+    ).toBeTruthy()
   })
 })
