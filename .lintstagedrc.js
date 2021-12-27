c9a9ef04b9db7f627be0d9d17a1133d6e74dee6d
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-12-27 12:45:08 +0000

    Chore: Add commitlint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,9 @@
 module.exports = {
+  // Lint all the staged files using Next (ESLint)
   '**/*.ts?(x)': (filenames) =>
     `next lint --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(' --file ')}`,
+  // Prettify the linted files using Prettier
   '**/*.{js,ts,tsx,json,css,md}"': 'prettier --write',
 }
