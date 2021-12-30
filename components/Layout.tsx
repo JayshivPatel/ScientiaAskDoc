4a5a5e653ca349e197e48eb69d9bad4273d2c11d
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-12-30 17:11:43 +0000

    Refactor: Move page metadata to constants folder

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,48 +1,12 @@
 import { Container, Footer, Logo } from '../styles/Layout.style'
-import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
+import Metadata from '../constants/Metadata'
 
-interface LayoutProps {
-  children: React.ReactNode
-}
-
-// TODO: Move page metadata to a constants folder and add a title prop
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container>
-      <Head>
-        <title>Scientia</title>
-        <meta name="description" content="A Unified DoC EdTech Platform" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="theme-color"
-          content="#000"
-          media="(prefers-color-scheme: dark)"
-        />
-        <meta
-          name="theme-color"
-          content="#fff"
-          media="(prefers-color-scheme: light)"
-        />
-      </Head>
+      <Metadata />
       {children}
       <Footer>
         <a
--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     // Files under these directories are linted using ESLint
-    dirs: ['pages', 'lib', 'components', 'tests'],
+    dirs: ['pages', 'lib', 'components', 'tests', 'constants'],
   },
 }
